f9e0d9546d9dc7afcd8ff9ec84802522067e8a6a
0x1m4o <chuntdevnx@gmail.com>
2023-05-20 15:08:52 +0700

    Filter Update

--- lib/cubits/cubit/todo_filter_cubit.dart
+++ lib/cubits/cubit/todo_filter_cubit.dart
@@ -7,6 +7,7 @@ part 'todo_filter_state.dart';
 class TodoFilterCubit extends Cubit<TodoFilterState> {
   TodoFilterCubit() : super(TodoFilterState.initial());
   // Emit the state
+  // Test
   void changeFilter(Filter newFilter) {
     emit(state.copyWith(filter: newFilter));
   }
